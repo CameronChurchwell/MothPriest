0eac2c21ba43e616deb4971b62258ab36282c95c
cameronchurchwell <steamtrains@icloud.com>
2023-04-19 22:37:15 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='MothPriest',
-    version='0.2',
+    version='0.3',
     description='Module for creating intuitive and efficient file parsers',
     author='Cameron Churchwell',
     author_email='cameronchurchwell2024@u.northwestern.edu',
