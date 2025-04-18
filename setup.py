22dfe9aa4a4644c987000a7916725417ac7d32a6
Cameron Churchwell <cameronchurchwell@icloud.com>
2025-04-18 03:01:29 +0000

    setup.py changes for pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='MothPriest',
-    version='0.6',
+    version='1.0.0',
     description='Module for creating intuitive and efficient file parsers',
     author='Cameron Churchwell',
-    author_email='cameronchurchwell2024@u.northwestern.edu',
+    author_email='cameronchurchwell@icloud.com',
     install_requires=[
         'pathlib',
         'typing',
